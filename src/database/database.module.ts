49af68fbb46a27087a21dfda09e823cc75c47a4e
israelhub <israelcunhaolive@gmail.com>
2025-05-29 17:31:58 -0300

    :sparkles: feat: add database module configuration with Sequelize

---
+++ src/database/database.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+@Module({
+  imports: [
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        dialect: 'postgres',
+        host: configService.get('DATABASE_HOST'),
+        port: configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USER'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        autoLoadModels: true,
+        synchronize: true,
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
