0d3df173cd91b430e685f20326a7af7633522520
IsraelHub <israelcunhaolive@gmail.com>
2025-06-02 15:43:59 -0300

    :sparkles: feat: Configure Swagger and validation pipes in the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { FolderModule } from './folder/folder.module';
 import { UserModule } from './user/user.module';
-import { BiographyModule } from './biography/biography.module';
-import { BiographyfolderModule } from './biographyfolder/biographyfolder.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    FolderModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     UserModule,
-    BiographyModule,
-    BiographyfolderModule,
     DatabaseModule,
   ],
   controllers: [AppController],
