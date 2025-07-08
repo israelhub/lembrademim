d8da200428c17bf71111a50fe1cced058472af7c
israelhub <israelcunhaolive@gmail.com>
2025-07-08 17:01:39 -0300

    🐛 fix: Add BiographyRepository and FolderRepository as providers in the corresponding modules

--- src/biography/biography.module.ts
+++ src/biography/biography.module.ts
@@ -3,11 +3,12 @@ import { BiographyService } from './biography.service';
 import { BiographyController } from './biography.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Biography } from './entities/biography.entity';
+import { BiographyRepository } from './biography.repository';
 
 @Module({
   imports: [SequelizeModule.forFeature([Biography])],
   controllers: [BiographyController],
-  providers: [BiographyService],
+  providers: [BiographyService, BiographyRepository],
   exports: [BiographyService],
 })
 export class BiographyModule {}
--- src/folder/folder.module.ts
+++ src/folder/folder.module.ts
@@ -3,11 +3,12 @@ import { FolderService } from './folder.service';
 import { FolderController } from './folder.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Folder } from './entities/folder.entity';
+import { FolderRepository } from './folder.repository';
 
 @Module({
   imports: [SequelizeModule.forFeature([Folder])],
   controllers: [FolderController],
-  providers: [FolderService],
+  providers: [FolderService, FolderRepository],
   exports: [FolderService],
 })
 export class FolderModule {}
